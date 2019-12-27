d25581dee623096b110c15f187ed2d85cdebe3b7
Pierre KORE <pierre.kore@infeeny.com>
2019-12-27 19:04:08 +0100

    Updates

---
+++ precache-manifest.2853958c477c761cc655d9ef58ac68bf.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e08bc6ef353498c1fe581bc42f2b2e2",
+    "url": "/Shadownsf/Portfolio/index.html"
+  },
+  {
+    "revision": "8fd19d90c20a4e3bd227",
+    "url": "/Shadownsf/Portfolio/static/css/2.400ebc83.chunk.css"
+  },
+  {
+    "revision": "153ff92e26385b8ebdd3",
+    "url": "/Shadownsf/Portfolio/static/css/main.1fb7086c.chunk.css"
+  },
+  {
+    "revision": "8fd19d90c20a4e3bd227",
+    "url": "/Shadownsf/Portfolio/static/js/2.8c2d361c.chunk.js"
+  },
+  {
+    "revision": "7bbee27d9c4808d828b85e5d24b408ae",
+    "url": "/Shadownsf/Portfolio/static/js/2.8c2d361c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "153ff92e26385b8ebdd3",
+    "url": "/Shadownsf/Portfolio/static/js/main.785e2826.chunk.js"
+  },
+  {
+    "revision": "5ba04b8cdf35624a3fd1",
+    "url": "/Shadownsf/Portfolio/static/js/runtime-main.02bf2e0b.js"
+  },
+  {
+    "revision": "e17e47d5dfdcea0c7bf2dfd6bf55f6d8",
+    "url": "/Shadownsf/Portfolio/static/media/.net.e17e47d5.png"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/Shadownsf/Portfolio/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "f935f7aa56e2ffe3d4d9a75180647d98",
+    "url": "/Shadownsf/Portfolio/static/media/azure.f935f7aa.png"
+  },
+  {
+    "revision": "6c2a3edb7069fe6d277cc68de57217f8",
+    "url": "/Shadownsf/Portfolio/static/media/azureDevOp.6c2a3edb.png"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/Shadownsf/Portfolio/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "be08914b5628819c66385fa8c6dd9ef0",
+    "url": "/Shadownsf/Portfolio/static/media/blop.be08914b.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/Shadownsf/Portfolio/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/Shadownsf/Portfolio/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/Shadownsf/Portfolio/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/Shadownsf/Portfolio/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/Shadownsf/Portfolio/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/Shadownsf/Portfolio/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "dec1fb9aff751f1969335a8f9b92ffb3",
+    "url": "/Shadownsf/Portfolio/static/media/lostTime.dec1fb9a.png"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/Shadownsf/Portfolio/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "891d200d1047e1c48913d469ad30e324",
+    "url": "/Shadownsf/Portfolio/static/media/mobiteach.891d200d.png"
+  },
+  {
+    "revision": "6dcf699938b2291d1634f91adb0dded1",
+    "url": "/Shadownsf/Portfolio/static/media/nodejs.6dcf6999.png"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/Shadownsf/Portfolio/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/Shadownsf/Portfolio/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "eeecf25b9df7058f6911b7038c7ed51c",
+    "url": "/Shadownsf/Portfolio/static/media/react.eeecf25b.png"
+  },
+  {
+    "revision": "5de754e3f257dd4db8fd58f0d7de14e9",
+    "url": "/Shadownsf/Portfolio/static/media/sqlServer.5de754e3.png"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "/Shadownsf/Portfolio/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/Shadownsf/Portfolio/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/Shadownsf/Portfolio/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/Shadownsf/Portfolio/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/Shadownsf/Portfolio/static/media/studio-5.ef5c30ea.jpg"
+  },
+  {
+    "revision": "304cb56de88f3bf57f8d7166996cd477",
+    "url": "/Shadownsf/Portfolio/static/media/unity3d.304cb56d.jpg"
+  }
+]);
